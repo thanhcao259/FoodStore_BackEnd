61a796fbd79e3049b7710c583867f584eb0147a6
thanhcao259 <thanhcao9501@gmail.com>
2024-06-17 12:47:20 +0700

    add allowedOrigins for FE

--- store/src/main/java/com/example/store/config/CorsConfig.java
+++ store/src/main/java/com/example/store/config/CorsConfig.java
@@ -14,6 +14,7 @@ public class CorsConfig implements WebMvcConfigurer {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("https://foodstore-two.vercel.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true)
