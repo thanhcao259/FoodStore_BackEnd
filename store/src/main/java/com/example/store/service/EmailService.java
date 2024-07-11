53c834ee1b891dff5145ff4826771087df8dfb12
thanhcao259 <thanhcao9501@gmail.com>
2024-07-11 09:34:33 +0700

    add service send HTML email

--- store/src/main/java/com/example/store/service/EmailService.java
+++ store/src/main/java/com/example/store/service/EmailService.java
@@ -1,25 +1,33 @@
 package com.example.store.service;
 
 import com.example.store.dto.MailBody;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+
+import java.util.Map;
 
 @Service
 public class EmailService {
 
     private static final Logger log = LoggerFactory.getLogger(EmailService.class);
-    private final JavaMailSender mailSender;
+    @Autowired
+    private JavaMailSender mailSender;
 
+    @Autowired
+    private SpringTemplateEngine templateEngine;
     @Value("${spring.mail.username}")
     private String fromEmail;
 
-    public EmailService(JavaMailSender mailSender) {
-        this.mailSender = mailSender;
-    }
 
     public void sendSimpleMessage(MailBody mailBody) {
         SimpleMailMessage message = new SimpleMailMessage();
@@ -32,5 +40,21 @@ public class EmailService {
         } catch (Exception e) {
             log.error("Authentication email failed: {}",e.getMessage());
         }
+    }
+
+    public void sendHTMLEmail (String to, String subject, Map<String, Object> model) throws MessagingException {
+        MimeMessage mess = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mess, true);
+
+        Context context = new Context();
+        context.setVariables(model);
+        String html = templateEngine.process("invoice_email", context);
+
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(html, true);
+        helper.setFrom(fromEmail);
+
+        mailSender.send(mess);
     }
 }
